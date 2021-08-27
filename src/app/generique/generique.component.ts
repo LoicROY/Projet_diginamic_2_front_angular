614e2fb10cc856e87c084d1c9256b528e643af28
Your Name <you@example.com>
2021-08-27 17:01:20 +0200

    -> css Nav bar+ Compter + sidebar

--- src/app/generique/generique.component.ts
+++ src/app/generique/generique.component.ts
@@ -5,10 +5,10 @@ import { TEXTE_HTML } from './../string/texteHTML';
 import { AppInjector } from '../app-injector';
 import { URL } from '../string/url';
 import { global } from '../string/ConstGlobal';
-import { HttpService } from '../service/httpService';
+import { HttpService } from '../service/HttpService';
 
 export class GeneriqueComponent {
-  
+
     public readonly TEXTE: any = TEXTE_HTML;
     public readonly REGEX: any = REGEX;
     public readonly ERREUR: any = ERREUR;
--- src/app/service/HttpService.ts
+++ src/app/service/HttpService.ts
@@ -1,19 +1,67 @@
-import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
+import { Departement } from './../models/departement';
+import { UserSignUp } from 'src/app/models/userSignUp';
+import { Absence } from 'src/app/models/absence';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { UserLogin } from "../models/userLogin";
+import { Salarie } from '../models/salarie';
 
 @Injectable({
-  providedIn:'root',
+    providedIn: 'root',
 })
 export class HttpService {
 
-  constructor(private http: HttpClient) {}
+    /**
+     * Url du backend
+     */
+    private backEndUrl: string = "http://localhost:8088/";
 
-  getSalarieCp(): Observable<number>{
-    return this.http.get<number>("http://localhost:8888/salarie/getSoldeCp");
-  }
-  getSalarieRTT(): Observable<number>{
-    return this.http.get<number>("http://localhost:8888/salarie/getSoldeRtt");
-  }
+    constructor(private http: HttpClient) { }
+
+    /**
+     * Methode d'authentification
+     */
+    public login(userLogin: UserLogin): Observable<any> {
+        const requestOptions: Object = {
+            responseType: 'text'
+        };
+        return this.http.post<string>(this.backEndUrl + "login", userLogin, requestOptions);
+    }
+
+    /**
+     * Methode de création d'un salarie ou manager ou admin
+     */
+    public signup(userSignUp: UserSignUp): Observable<any> {
+        return this.http.post<string>(this.backEndUrl + userSignUp.type + '/create', userSignUp);
+    }
+
+    /**
+     * Enregistrement d'une absence
+     */
+    public postAbsence(absence: Absence): Observable<Absence> {
+        return this.http.post<Absence>(this.backEndUrl + absence.type, absence);
+    }
+
+    /**
+     * Récupère la liste des salariés en base
+     */
+    public getSalaries(): Observable<Salarie[]> {
+        return this.http.get<Salarie[]>(this.backEndUrl + "Salarie");
+    }
+
+    /**
+     * Modification d'une absence en base
+     */
+    public putAbsence(absence: Absence): Observable<Absence> {
+        return this.http.put<Absence>(this.backEndUrl + absence.type, absence);
+    }
+
+    /**
+     * Récupère la liste des departements en base
+     */
+    public getAllDepartements(): Observable<Departement[]> {
+        return this.http.get<Departement[]>(this.backEndUrl + 'Departement/getAll');
+    }
 
 }
